b83d1635e0b12aeeec463f97f65e15dc6d18cfc4
gauravwani-coditas <gaurav.wani@coditas.com>
2024-03-12 23:17:35 +0530

    Progress Bar added

--- lib/starting_screen.dart
+++ lib/starting_screen.dart
@@ -24,6 +24,9 @@ class _StartingState extends State<StartingScreen> {
 
   List<String> laps = [];
 
+  bool recordButtonVisibility = false;
+  bool lapsVisibility = false;
+
   Widget playIcon = const Icon(
     Icons.play_arrow,
     color: Color.fromARGB(132, 0, 0, 0),
@@ -51,6 +54,7 @@ class _StartingState extends State<StartingScreen> {
   void onStart() {
     //changing current condition
     startPause = "Pause";
+    recordButtonVisibility = true;
     timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       updateTimer();
     });
@@ -62,6 +66,7 @@ class _StartingState extends State<StartingScreen> {
     timer!.cancel();
     setState(() {
       startPause = "Start";
+      recordButtonVisibility = false;
     });
   }
 
@@ -70,6 +75,8 @@ class _StartingState extends State<StartingScreen> {
 
     setState(() {
       startPause = "Start";
+      recordButtonVisibility = false;
+      lapsVisibility = false;
       seconds = 0;
       minutes = 0;
       hours = 0;
@@ -87,6 +94,7 @@ class _StartingState extends State<StartingScreen> {
       // digitalSeconds = seconds < 10 ? "0$seconds" : "$seconds";
       // digitalMinutes = minutes < 10 ? "0$minutes" : "$minutes";
       // digitalHours = hours < 10 ? "0$hours" : "$hours";
+      lapsVisibility = true;
       laps.add("$digitalHours $digitalMinutes $digitalSeconds");
     });
   }
@@ -112,101 +120,125 @@ class _StartingState extends State<StartingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          const Text(
-            "STOPWATCH",
-            style: TextStyle(color: Color.fromARGB(132, 0, 0, 0), fontSize: 24),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20),
-            alignment: Alignment.center,
-            height: 300,
-            decoration: const  BoxDecoration(
-              shape: BoxShape.circle,
-              color:  Color.fromARGB(255, 156, 184, 249),
-
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 40,
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
+            const Text(
+              "STOPWATCH",
+              style:
+                  TextStyle(color: Color.fromARGB(132, 0, 0, 0), fontSize: 24),
+            ),
+            Stack(
+              alignment: Alignment.center,
               children: [
-                Text(
-                  "$digitalHours :  $digitalMinutes :  $digitalSeconds",
-                  style: const TextStyle(
-                      fontSize: 40, color: Color.fromARGB(132, 0, 0, 0)),
+                Container(
+                  margin: const EdgeInsets.all(20),
+                  alignment: Alignment.center,
+                  height: 300,
+                  width: 300,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 156, 184, 249),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "$digitalHours :  $digitalMinutes :  $digitalSeconds",
+                        style: const TextStyle(
+                            fontSize: 40, color: Color.fromARGB(132, 0, 0, 0)),
+                      ),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: SizedBox(
+                    height: 300,
+                    width: 300,
+                    child: CircularProgressIndicator(
+                      value: seconds / 60,
+                      valueColor: const AlwaysStoppedAnimation<Color>(
+                        Color.fromARGB(255, 0, 16, 53),
+                      ),
+                      strokeWidth: 20,
+                    ),
+                  ),
                 ),
               ],
             ),
-          ),
-
-         
-
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ElevatedButton(
-                    onPressed: startPause == "Start" ? onStart : onStop,
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color.fromARGB(255, 156, 184, 249)),
-                    child: startPause == "Start" ? playIcon : pauseIcon,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: onReset,
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color.fromARGB(255, 156, 184, 249)),
-                    child: resetIcon,
-                  ),
-                  const SizedBox(
-                    width: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: onRecord,
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            const Color.fromARGB(255, 156, 184, 249)),
-                    child: recordIcon,
-                  ),
-                ],
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      onPressed: startPause == "Start" ? onStart : onStop,
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 156, 184, 249)),
+                      child: startPause == "Start" ? playIcon : pauseIcon,
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: onReset,
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 156, 184, 249)),
+                      child: resetIcon,
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    Visibility(
+                      visible: recordButtonVisibility,
+                      child: ElevatedButton(
+                        onPressed: onRecord,
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 156, 184, 249)),
+                        child: recordIcon,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text(
-            "LAPS",
-            style: TextStyle(fontSize: 24, color: Color.fromARGB(132, 0, 0, 0)),
-          ),
-          Expanded(
-            child: ListView(
-              padding: const EdgeInsets.all(8),
-              children: laps.map((element) {
-                return Container(
-                  margin: const EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 156, 184, 249),
-                  ),
-                  padding: const EdgeInsets.all(8),
-                  child: Text(element),
-                );
-              }).toList(),
+            const SizedBox(
+              height: 20,
             ),
-          ),
-        ],
+            Visibility(
+              visible: lapsVisibility,
+              child: const Text(
+                "LAPS",
+                style: TextStyle(
+                    fontSize: 24, color: Color.fromARGB(132, 0, 0, 0)),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: laps.map((element) {
+                  return Container(
+                    margin: const EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromARGB(255, 156, 184, 249),
+                    ),
+                    padding: const EdgeInsets.all(8),
+                    child: Text(element),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
